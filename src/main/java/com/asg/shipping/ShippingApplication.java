1a91437fdd02c2504f379f605756404de1917223
Sridharan-Official <srajendaran@hexalytics.com>
2025-12-19 17:59:48 +0530

    Implemented the Api's and its test cases for Port Master

--- src/main/java/com/asg/shipping/ShippingApplication.java
+++ src/main/java/com/asg/shipping/ShippingApplication.java
@@ -2,8 +2,14 @@ package com.asg.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.asg" })
+@EnableJpaRepositories(basePackages = { "com.asg.common.lib.repository", "com.asg.shipping" })
+@EntityScan(basePackages = { "com.asg.common.lib.entity", "com.asg.shipping" })
 public class ShippingApplication {
 
 	public static void main(String[] args) {
