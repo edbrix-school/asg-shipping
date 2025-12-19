61850fe1d193a14ffb9a353ca183dc7186fe6504
Sridharan-Official <srajendaran@hexalytics.com>
2025-12-19 17:59:48 +0530

    Implemented the Api's and its test cases for Port Master

---
+++ src/main/java/com/asg/shipping/portMaster/repository/PortMasterRepository.java
@@ -0,0 +1,23 @@
+package com.asg.shipping.portMaster.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.asg.shipping.portMaster.entity.PortMaster;
+import com.asg.shipping.portMaster.entity.PortMasterId;
+
+public interface PortMasterRepository extends JpaRepository<PortMaster, PortMasterId> {
+
+	Optional<PortMaster> findByGroupPoidAndPortCode(Long groupPoid, String portCode);
+
+	Optional<PortMaster> findByGroupPoidAndPortName(Long groupPoid, String portName);
+
+	List<PortMaster> findByGroupPoidAndDeletedNot(Long groupPoid, String deleted);
+
+	Optional<PortMaster> findByPortCode(String portCode);
+
+	Optional<PortMaster> findByPortName(String portName);
+
+}
