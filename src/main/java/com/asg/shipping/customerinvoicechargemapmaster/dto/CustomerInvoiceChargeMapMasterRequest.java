a31987c88e131395384af7e738811c343836e00f
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2025-12-22 23:26:41 +0530

    done some changes

--- src/main/java/com/asg/shipping/customerinvoicechargemapmaster/dto/CustomerInvoiceChargeMapMasterRequest.java
+++ src/main/java/com/asg/shipping/customerinvoicechargemapmaster/dto/CustomerInvoiceChargeMapMasterRequest.java
@@ -1,5 +1,9 @@
 package com.asg.shipping.customerinvoicechargemapmaster.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +12,14 @@ import java.util.List;
 @Getter
 @Setter
 public class CustomerInvoiceChargeMapMasterRequest {
+
+    @NotNull
+    @Positive
     private Long customerPoid;
+
+    @NotEmpty
+    @Valid
     private List<CustomerInvoiceChargeMapDetailDto> details;
 }
+
 
