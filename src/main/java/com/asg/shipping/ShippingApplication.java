1dacacbaa7513afd7911be07aab82f885a472154
Ajith Kumar <aandiappan@hexalytics.com>
2025-12-22 23:09:47 +0530

    containertypes and groupcontainertypes apis

--- src/main/java/com/asg/shipping/ShippingApplication.java
+++ src/main/java/com/asg/shipping/ShippingApplication.java
@@ -2,8 +2,20 @@ package com.asg.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.asg"})
+@EnableJpaRepositories(basePackages = {
+		"com.asg.shipping",
+		"com.asg.common.lib.repository"
+})
+@EntityScan(basePackages = {
+		"com.asg.shipping",
+		"com.asg.common.lib.entity"
+})
 public class ShippingApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/asg/shipping/containertypes/service/ContainerTypeService.java
@@ -0,0 +1,70 @@
+package com.asg.shipping.containertypes.service;
+
+import com.asg.common.lib.dto.FilterRequestDto;
+import com.asg.shipping.containertypes.dto.ContainerTypeCreateDTO;
+import com.asg.shipping.containertypes.dto.ContainerTypeDto;
+import com.asg.shipping.containertypes.dto.ContainerTypeUpdateDTO;
+
+import java.util.Map;
+
+/**
+ * Service interface for Container Type operations
+ */
+public interface ContainerTypeService {
+
+    /**
+     * Search/list container types with filters and pagination
+     * Uses DocumentSearchService for unified search functionality
+     *
+     * @param docId Document ID (e.g., "100-002" for Container Type)
+     * @param request FilterRequestDto containing filters, operator, isDeleted
+     * @param pageable Pagination and sorting information
+     * @return Map containing paginated results and display fields
+     */
+    Map<String, Object> searchContainerTypes(String docId, FilterRequestDto request, org.springframework.data.domain.Pageable pageable);
+
+    /**
+     * Get container type by ID
+     *
+     * @param id Container Type POID
+     * @return ContainerTypeDto
+     */
+    ContainerTypeDto getContainerType(Long id);
+
+    /**
+     * Create a new container type
+     *
+     * @param dto Container type creation data
+     * @param groupPoid Group POID from UserContext
+     * @param userPoid User POID from UserContext
+     * @return Created ContainerTypeDto
+     */
+    ContainerTypeDto createContainerType(ContainerTypeCreateDTO dto, Long groupPoid, Long userPoid);
+
+    /**
+     * Update an existing container type
+     *
+     * @param id Container Type POID
+     * @param dto Container type update data
+     * @param groupPoid Group POID from UserContext
+     * @param userPoid User POID from UserContext
+     * @return Updated ContainerTypeDto
+     */
+    ContainerTypeDto updateContainerType(Long id, ContainerTypeUpdateDTO dto, Long groupPoid, Long userPoid);
+
+    /**
+     * Toggle active status of a container type
+     *
+     * @param id Container Type POID
+     */
+    void toggleActive(Long id);
+
+    /**
+     * Soft delete a container type
+     *
+     * @param id Container Type POID
+     */
+    void deleteContainerType(Long id);
+}
+
+
