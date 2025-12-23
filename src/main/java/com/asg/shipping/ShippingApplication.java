3af502d58cb1f7d14a5cf6a47a5929bdd47d9d71
Hamsana <hamsanadurai26@gmail.com>
2025-12-23 13:15:18 +0530

    next line

--- src/main/java/com/asg/shipping/ShippingApplication.java
+++ src/main/java/com/asg/shipping/ShippingApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication@ComponentScan(basePackages = {"com.asg"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.asg"})
 @EnableJpaRepositories(basePackages = {"com.asg.common.lib.repository", "com.asg.shipping"})
 @EntityScan(basePackages = {"com.asg.common.lib.entity", "com.asg.shipping"})
 public class ShippingApplication {
