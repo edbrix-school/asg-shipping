bf3d611964a4afc19807e23e72b78c350e820d55
Vaikundamuthu <vkannan@hexalytics.com>
2026-01-05 18:40:20 +0530

    Used trigger to generate doc ref and transaction poid

--- src/main/java/com/asg/shipping/containertypeportchargestariff/repository/PortChargesTariffCustomRepository.java
+++
@@ -1,5 +0,0 @@
-package com.asg.shipping.containertypeportchargestariff.repository;
-
-public interface PortChargesTariffCustomRepository {
-    void copyPortChargesTariff(Long sourceTransactionPoid);
-}
