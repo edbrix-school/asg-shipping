ebb782b21d072a6e818f87493c54f69d1a0c5168
Sridharan-Official <srajendaran@hexalytics.com>
2026-03-09 15:46:19 +0530

    Fixed the Api issue in the Day close shipping

--- src/main/java/com/asg/shipping/MafiTrailerDateUpdateForm/entity/ShipBlMafiHdr.java
+++ src/main/java/com/asg/shipping/MafiTrailerDateUpdateForm/entity/ShipBlMafiHdr.java
@@ -1,8 +1,6 @@
 package com.asg.shipping.MafiTrailerDateUpdateForm.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
+import com.asg.common.lib.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -11,50 +9,41 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "SHIP_BL_MAFI_HDR")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ShipBlMafiHdr {
+public class ShipBlMafiHdr extends BaseEntity {
 
-	@Id
-	@Column(name = "TRANSACTION_POID")
-	private Long transactionPoid;
+    @Id
+    @Column(name = "TRANSACTION_POID")
+    private Long transactionPoid;
 
-	@Column(name = "GROUP_POID")
-	private Long groupPoid;
+    @Column(name = "GROUP_POID")
+    private Long groupPoid;
 
-	@Column(name = "COMPANY_POID")
-	private Long companyPoid;
+    @Column(name = "COMPANY_POID")
+    private Long companyPoid;
 
-	@Column(name = "TRANSACTION_DATE")
-	private LocalDate transactionDate;
+    @Column(name = "TRANSACTION_DATE")
+    private LocalDate transactionDate;
 
-	@Column(name = "DOC_REF", unique = true)
-	private String docRef;
+    @Column(name = "DOC_REF", unique = true)
+    private String docRef;
 
-	@Column(name = "VOYAGE_TRANSACTION_POID")
-	private Long voyageTransactionPoid;
+    @Column(name = "VOYAGE_TRANSACTION_POID")
+    private Long voyageTransactionPoid;
 
-	@Column(name = "AGENT_REFERENCE", length = 200)
-	private String agentReference;
+    @Column(name = "AGENT_REFERENCE", length = 200)
+    private String agentReference;
 
-	@Column(name = "REMARKS", length = 200)
-	private String remarks;
+    @Column(name = "REMARKS", length = 200)
+    private String remarks;
 
-	@Column(name = "DELETED", length = 10)
-	private String deleted;
+    @Column(name = "DELETED", length = 10)
+    private String deleted;
 
-	@Column(name = "CREATED_BY", length = 20)
-	private String createdBy;
-
-	@Column(name = "CREATED_DATE")
-	private LocalDateTime createdDate;
-
-	@Column(name = "LASTMODIFIED_BY", length = 20)
-	private String lastModifiedBy;
-
-	@Column(name = "LASTMODIFIED_DATE")
-	private LocalDateTime lastModifiedDate;
 }
