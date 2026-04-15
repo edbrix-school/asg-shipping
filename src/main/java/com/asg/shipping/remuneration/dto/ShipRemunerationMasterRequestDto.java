79131a9b541df43b929171feeafb7160d437c74e
Vaikundamuthu <vkannan@hexalytics.com>
2026-04-16 00:15:10 +0530

    Refactored the validations of remuneration master

--- src/main/java/com/asg/shipping/remuneration/dto/ShipRemunerationMasterRequestDto.java
+++ src/main/java/com/asg/shipping/remuneration/dto/ShipRemunerationMasterRequestDto.java
@@ -1,15 +1,18 @@
 package com.asg.shipping.remuneration.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ShipRemunerationMasterRequestDto {
+    @Size(max = 20, message = "Remuneration Code must not exceed 20 characters")
     @NotBlank(message = "Remuneration Code should not be empty")
     private String remunCode;
 
+    @Size(max = 100, message = "Remuneration Description must not exceed 100 characters")
     @NotBlank(message = "Remuneration Description should not be empty")
     private String remunDescription;
 
