0be89131e01036d82ef6800d070a87123477ba6f
spsingh89 <spsingh@hexalytics.com>
2026-06-19 18:41:36 +0530

    created list API for delivery order issue

--- src/main/java/com/asg/shipping/deliveryorderissuetocustomer/service/DeliveryOrderIssueToCustomerService.java
+++ src/main/java/com/asg/shipping/deliveryorderissuetocustomer/service/DeliveryOrderIssueToCustomerService.java
@@ -3,6 +3,9 @@ package com.asg.shipping.deliveryorderissuetocustomer.service;
 import com.asg.shipping.deliveryorderissuetocustomer.dto.*;
 import com.asg.shipping.deliveryorderissuetocustomer.enums.ButtonType;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Map;
 
 public interface DeliveryOrderIssueToCustomerService {
     DeliveryOrderIssueToCustomerDto getDeliveryOrderIssueToCustomer(Long id);
@@ -14,4 +17,6 @@ public interface DeliveryOrderIssueToCustomerService {
     byte[] print(Long transactionPoid, @Valid IssueDeliveryOrderRequestDto requestDto, ButtonType buttonType) throws Exception;
 
     ValidateDocumentDto validateDocument(Long id, @Valid IssueDeliveryOrderRequestDto requestDto);
+    
+    Map<String, Object> searchDeliveryOrders(String documentId, com.asg.common.lib.dto.FilterRequestDto filters, Pageable pageable);
 }
