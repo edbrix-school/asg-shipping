1f5d6b5c092e9b22de7b3333d57f22690c6184f8
AKhilesh-Dwivedi <adwivedi@hexalytics.com>
2026-07-03 15:26:32 +0530

    fix Issue ASAS-4526

--- src/test/java/com/asg/shipping/containerinventorymovementupdate/CimuServiceImplTest.java
+++ src/test/java/com/asg/shipping/containerinventorymovementupdate/CimuServiceImplTest.java
@@ -262,7 +262,6 @@ class CimuServiceImplTest {
     // ---------- calculateDemurrage ----------
 
     @Test
-    @Disabled
     void calculateDemurrage_success() {
         DemurrageCalculateRequest request = new DemurrageCalculateRequest();
         request.setTransactionPoid(100L);
@@ -273,13 +272,14 @@ class CimuServiceImplTest {
                 .thenReturn(BigDecimal.TEN);
         when(demurrageRepository.getImportTotalMessage(any(), any(), any()))
                 .thenReturn("Total amount need to collect =10");
-        when(queryRepository.fetchTotalCollectedAmount(any(), any()))
-                .thenReturn(BigDecimal.ONE);
+        when(demurrageRepository.getPortDays(any(), any(), any()))
+                .thenReturn(BigDecimal.valueOf(5));
 
         DemurrageCalculateResponse response = service.calculateDemurrage(request);
 
         assertEquals(BigDecimal.TEN, response.getDemurrageAmount());
-        assertEquals(BigDecimal.ONE, response.getTotalCollectedAmount());
+        assertEquals(BigDecimal.valueOf(5), response.getPortDays());
+        assertNotNull(response.getCollectedSummaryMessage());
     }
 
     @Test
@@ -293,14 +293,24 @@ class CimuServiceImplTest {
     }
 
     @Test
-    @Disabled
     void calculateDemurrage_previousDate() {
         DemurrageCalculateRequest request = new DemurrageCalculateRequest();
         request.setTransactionPoid(100L);
         request.setContainerNo("CONT001");
         request.setDemDt(LocalDate.now().minusDays(1));
 
-        assertThrows(ValidationException.class, () -> service.calculateDemurrage(request));
+        when(demurrageRepository.calculateDemurrage(any(), any(), any()))
+                .thenReturn(BigDecimal.ZERO);
+        when(demurrageRepository.getImportTotalMessage(any(), any(), any()))
+                .thenReturn(null);
+        when(demurrageRepository.getPortDays(any(), any(), any()))
+                .thenReturn(BigDecimal.ZERO);
+
+        DemurrageCalculateResponse response = service.calculateDemurrage(request);
+
+        assertNotNull(response);
+        assertEquals(BigDecimal.ZERO, response.getDemurrageAmount());
+        assertEquals(BigDecimal.ZERO, response.getPortDays());
     }
 
     // ---------- importFile ----------
