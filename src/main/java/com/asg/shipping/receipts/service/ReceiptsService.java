5c1c936d1093b7a1666aeb2e0ad1461a3436a349
Sahil Khurana <skhurana@hexalytics.com>
2026-07-24 12:41:40 +0530

    AAD-1660 Receipts (Shipping) LOV date filter working

--- src/main/java/com/asg/shipping/receipts/service/ReceiptsService.java
+++ src/main/java/com/asg/shipping/receipts/service/ReceiptsService.java
@@ -7,6 +7,7 @@ import com.asg.shipping.receipts.enums.ButtonType;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 public interface ReceiptsService {
@@ -19,7 +20,7 @@ public interface ReceiptsService {
 
 	void deleteReceipt(Long transactionPoid, DeleteReasonDto deleteReasonDto);
 
-	Map<String, Object> list(FilterRequestDto filters, Pageable pageable);
+	Map<String, Object> list(FilterRequestDto filters, Pageable pageable, LocalDate startDate, LocalDate endDate);
 
 	ReceiptAutoPopulateDto autoPopulateFields(Long blPoid,Long transactionPoid);
 
