de0c768c6d92241add73d35f3a67888fa0d8507c
Harsh Taneja <htaneja@hexalytics.com>
2026-08-07 14:49:51 +0530

    Added native querry for lovs to improve performace

--- src/main/java/com/asg/shipping/importmanifestupdate/respository/ShipBlManifestChargesDtlRepository.java
+++ src/main/java/com/asg/shipping/importmanifestupdate/respository/ShipBlManifestChargesDtlRepository.java
@@ -26,19 +26,18 @@ public interface ShipBlManifestChargesDtlRepository  extends JpaRepository<ShipB
 
     /**
      * Replaces the MANIFEST_RECEIPT_INVOICE LOV lookup by POID with a direct query against the source tables.
+     * The original LOV's DOC_REF_LINK_NO/SUBSTR gating only makes sense for the browse/search path
+     * (finding which charges reference a given receipt/invoice); resolving an already-known
+     * receiptInvoicePoid just needs a direct header lookup, independent of any charges-detail row.
      */
     @Query(value = """
-        SELECT DISTINCT recphdr.TRANSACTION_POID AS POID, recphdr.DOC_REF AS CODE, SUBSTR(dtl.DOC_REF_LINK_NO, 15, 7) AS DESCRIPTION
-        FROM SHIP_BL_MANIFEST_CHARGES_DTL dtl
-        INNER JOIN AR_SH_RECEIPT_HDR recphdr ON dtl.RECEIPT_INVOICE_POID = recphdr.TRANSACTION_POID
-        WHERE dtl.RECEIPT_INVOICE_POID IN (:poids)
-          AND SUBSTR(dtl.DOC_REF_LINK_NO, 15, 7) = '300-103'
+        SELECT TRANSACTION_POID AS POID, DOC_REF AS CODE, '300-103' AS DESCRIPTION
+        FROM AR_SH_RECEIPT_HDR
+        WHERE TRANSACTION_POID IN (:poids)
         UNION ALL
-        SELECT DISTINCT invhdr.TRANSACTION_POID AS POID, invhdr.DOC_REF AS CODE, SUBSTR(dtl.DOC_REF_LINK_NO, 15, 7) AS DESCRIPTION
-        FROM SHIP_BL_MANIFEST_CHARGES_DTL dtl
-        INNER JOIN AR_SH_SALES_INVOICE_HDR invhdr ON dtl.RECEIPT_INVOICE_POID = invhdr.TRANSACTION_POID
-        WHERE dtl.RECEIPT_INVOICE_POID IN (:poids)
-          AND SUBSTR(dtl.DOC_REF_LINK_NO, 15, 7) = '300-102'
+        SELECT TRANSACTION_POID AS POID, DOC_REF AS CODE, '300-102' AS DESCRIPTION
+        FROM AR_SH_SALES_INVOICE_HDR
+        WHERE TRANSACTION_POID IN (:poids)
         """, nativeQuery = true)
     List<Object[]> findReceiptInvoiceLovByPoids(@Param("poids") List<Long> poids);
 }
